6b6dbb705b6aa346e30cfa1d956b2c159f9a29f9
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-01-15 16:44:27 +0100

    Add first React Article and second react code

---
+++ react/react-basics/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>This is my second React code. Yippie!</p>
+    </article>
+  );
+}
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
